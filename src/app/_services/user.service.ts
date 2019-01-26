b79f98f8699983c9ffb69bd6052cb880d3d307e7
baumhoto <baumhoto@gmail.com>
2019-01-26 12:52:17 +0100

    - use jwtmodule to automatically send token - configure jwtmodule in app.module.ts - removed old token attach from user.service

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -2,14 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
 import { User } from '../_models/user';
-
-const httpsOptions = {
-  headers: new HttpHeaders({
-    'Authorization': 'Bearer ' + localStorage.getItem('token')
-  })
-};
 
 @Injectable({
   providedIn: 'root'
@@ -20,10 +13,10 @@ export class UserService {
   constructor(private http: HttpClient) { }
 
   public getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this.baseUrl, httpsOptions);
+    return this.http.get<User[]>(this.baseUrl);
   }
 
   public getUser(id: number): Observable<User> {
-    return this.http.get<User>(this.baseUrl + id, httpsOptions);
+    return this.http.get<User>(this.baseUrl + id);
   }
 }
