c0b78e3b98c25c15d021a23787ff84aa38f8f204
baumhoto <baumhoto@gmail.com>
2019-01-28 11:52:45 +0100

    - added ng2-file-upload package - use ng2-file-upload in PhotoEditor to enable user to upload photos

--- src/app/members/PhotoEditor/PhotoEditor.component.ts
+++ src/app/members/PhotoEditor/PhotoEditor.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
+
 import { Photo } from 'src/app/_models/photo';
+import { AuthService } from 'src/app/_services/auth.service';
+import { environment } from 'src/environments/environment';
+import { rS } from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-PhotoEditor',
@@ -8,10 +13,46 @@ import { Photo } from 'src/app/_models/photo';
 })
 export class PhotoEditorComponent implements OnInit {
   @Input() photos: Photo[];
+  uploader: FileUploader;
+  hasBaseDropZoneOver = false;
+  baseUrl = environment.apiUrl + 'users/';
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.initializeUploader();
+  }
+
+  fileOverBase(e: any): void {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  initializeUploader() {
+    this.uploader = new FileUploader({
+      url: this.baseUrl + this.authService.decodedToken.nameid + '/photos',
+      authToken: 'Bearer ' + localStorage.getItem('token'),
+      isHTML5: true,
+      allowedFileType: ['image'],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 10 * 1024 * 1024
+    });
+
+    this.uploader.onAfterAddingFile = (file) => {file.withCredentials = false; }
+
+    this.uploader.onSuccessItem = (item, response, status, headers) => {
+      if (response) {
+        const res: Photo = JSON.parse(response);
+        const photo = {
+          id: res.id,
+          url: res.url,
+          dateAdded: res.dateAdded,
+          description: res.description,
+          isMain: res.isMain
+        };
+        this.photos.push(photo);
+      }
+    };
   }
 
 }
