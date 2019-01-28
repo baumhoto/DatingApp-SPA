b2b68ed55600542849dae1b8074b05c2379baee2
baumhoto <baumhoto@gmail.com>
2019-01-28 17:05:47 +0100

    - added delete photo in photo-editor component

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -27,4 +27,8 @@ export class UserService {
   setMainPhoto(userId: number, id: number) {
     return this.http.post(this.baseUrl + userId + '/photos/' + id + '/setMain', {});
   }
+
+  deletePhoto(userId: number, id: number) {
+    return this.http.delete(this.baseUrl + userId + '/photos/' + id);
+  }
 }
